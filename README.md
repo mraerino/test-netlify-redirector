7518b989aba3f62c60c764cc3676176462346ebf
erezrokah <erezrokah@users.noreply.github.com>
2020-11-09 20:32:01 +0100

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Setup
+
+```sh
+npm ci
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,31 @@
+const redirector = require("netlify-redirector");
+
+const test = async () => {
+  const matcher = await redirector.parsePlain(
+    "/admin/* /admin/:splat 200 Role=admin",
+    {
+      jwtSecret: "secret",
+      jwtRole: "app_metadata.authorization.roles",
+    }
+  );
+
+  const nf_jwt =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjIxNDkwMTc5NTYwMDAsImFwcF9tZXRhZGF0YSI6eyJhdXRob3JpemF0aW9uIjp7InJvbGVzIjpbImFkbWluIl19fX0.m7RC0H0NjZ23SKT4IdJpaWTMwTZxIG_rvw72nZEq-84";
+  const request = {
+    scheme: "http",
+    host: "localhost",
+    path: "/admin/foo",
+    headers: {
+      host: "localhost:8888",
+      cookie: `nf_jwt=${nf_jwt}`,
+    },
+    cookieValues: {
+      nf_jwt,
+    },
+  };
+
+  const match = matcher.match(request);
+  console.log(match);
+};
+
+test();
