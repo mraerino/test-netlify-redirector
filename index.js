a5d598aea1171e9f7e468c5e139622c687abfd00
erezrokah <erezrokah@users.noreply.github.com>
2020-11-10 11:34:34 +0100

    fix: pass cookies prop

--- index.js
+++ index.js
@@ -11,17 +11,22 @@ const test = async () => {
 
   const nf_jwt =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjIxNDkwMTc5NTYwMDAsImFwcF9tZXRhZGF0YSI6eyJhdXRob3JpemF0aW9uIjp7InJvbGVzIjpbImFkbWluIl19fX0.m7RC0H0NjZ23SKT4IdJpaWTMwTZxIG_rvw72nZEq-84";
+  const cookieValues = {
+    nf_jwt,
+  };
+  const headers = {
+    host: "localhost:8888",
+    cookie: `nf_jwt=${nf_jwt}`,
+  };
   const request = {
     scheme: "http",
     host: "localhost",
     path: "/admin/foo",
-    headers: {
-      host: "localhost:8888",
-      cookie: `nf_jwt=${nf_jwt}`,
-    },
-    cookieValues: {
-      nf_jwt,
-    },
+    headers,
+    cookies: {},
+    cookieValues,
+    getHeader: (name) => headers[name.toLowerCase()] || "",
+    getCookie: (key) => cookieValues[key] || "",
   };
 
   const match = matcher.match(request);
